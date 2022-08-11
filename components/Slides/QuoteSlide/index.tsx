a4f5972a7271b69d9cb901751f05c9b203f1d0e0
IgnacioCurti <ignaciocurtijristo@gmail.com>
2022-08-11 10:39:32 -0300

    turned quoteSlide into iFrameSlide

--- components/Slides/QuoteSlide/index.tsx
+++ components/Slides/QuoteSlide/index.tsx
@@ -6,43 +6,60 @@ import styles from "./QuoteSlide.module.scss";
 /**
  * The quote slide component
  */
-export default function QuoteSlide(props: QuoteSlideProps): JSX.Element {
-    const email = PrismicDOM.RichText.asText(props.content.slide_email);
+// export default function QuoteSlide(props: QuoteSlideProps): JSX.Element {
+//     const email = PrismicDOM.RichText.asText(props.content.slide_email);
 
+//     return (
+//         <div className={styles.content}>
+//             <div className={styles.leftSide}>
+//                 <Image
+//                     width={125}
+//                     height={125}
+//                     src={props.content.slide_thumbnail.url!}
+//                     alt={props.content.slide_thumbnail.alt!}
+//                 />
+
+//                 <h3>{props.content.slide_name[0].text}</h3>
+//                 <p className="subtitle-small">
+//                     {PrismicDOM.RichText.asText(props.content.slide_title)}
+//                 </p>
+//             </div>
+
+//             <span className={styles.separator} />
+
+//             <div className={styles.rightSide}>
+//                 <div
+//                     className={styles.quote}
+//                     dangerouslySetInnerHTML={{
+//                         __html: PrismicDOM.RichText.asHtml(props.content.slide_quote)
+//                     }}
+//                 />
+
+//                 <a href={`mailto:${email}`} className={styles.email}>
+//                     {email}
+//                 </a>
+
+//                 <span className={styles.phone}>
+//                     {PrismicDOM.RichText.asText(props.content.slide_phone)}
+//                 </span>
+//             </div>
+//         </div>
+//     );
+// }
+
+/**
+ * The quote slide component
+ */
+export default function QuoteSlide(props: QuoteSlideProps): JSX.Element {
     return (
         <div className={styles.content}>
-            <div className={styles.leftSide}>
-                <Image
-                    width={125}
-                    height={125}
-                    src={props.content.slide_thumbnail.url!}
-                    alt={props.content.slide_thumbnail.alt!}
-                />
-
-                <h3>{props.content.slide_name[0].text}</h3>
-                <p className="subtitle-small">
-                    {PrismicDOM.RichText.asText(props.content.slide_title)}
-                </p>
-            </div>
-
-            <span className={styles.separator} />
-
-            <div className={styles.rightSide}>
-                <div
-                    className={styles.quote}
-                    dangerouslySetInnerHTML={{
-                        __html: PrismicDOM.RichText.asHtml(props.content.slide_quote)
-                    }}
-                />
-
-                <a href={`mailto:${email}`} className={styles.email}>
-                    {email}
-                </a>
-
-                <span className={styles.phone}>
-                    {PrismicDOM.RichText.asText(props.content.slide_phone)}
-                </span>
-            </div>
+            <iframe
+                // style="border: 1px solid rgba(0, 0, 0, 0.1);"
+                width="1300"
+                height="600"
+                src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FCsB8pjCjnRTN6b2534L8hO%2FProde-Qatar-2022%3Fnode-id%3D0%253A1"
+                allowFullScreen
+            ></iframe>
         </div>
     );
 }
